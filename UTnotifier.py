20b6fac83017169b4f6a8a79802af0a7e2d7dd36
pacjo <56438628+pacjo@users.noreply.github.com>
2022-02-10 16:20:42 +0100

    added few checks, script will try to log in with the credentials.json

--- UTnotifier.py
+++ UTnotifier.py
@@ -1,7 +1,10 @@
 import os
 import time
+import json
+import inspect
 from datetime import datetime
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from colorama import init, Fore
 from plyer.utils import platform
 from plyer import notification
@@ -20,11 +23,33 @@ print(Fore.MAGENTA + "\n https://github.com/pacjo/UTnotifier \n")
 # WebDriver initialization
 options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
-driver = webdriver.Chrome(options=options)
+
+try:
+    driver = webdriver.Chrome(options=options)
+except:
+  print(Fore.RED + "Chrome WebDriver doesn't appear to be available. Make sure it's in PATH or in the script directory")
+  exit()
 
 # Access UT account
 driver.get('https://app.usertesting.com/my_dashboard/available_tests_v3');
-print(Fore.GREEN + "Log in to continue...")
+
+filename = inspect.getframeinfo(inspect.currentframe()).filename
+file_path = os.path.dirname(os.path.abspath(filename))
+
+try:
+# if os.path.exists(f'{file_path}/credentials.json'):
+    file = open(f'{file_path}/credentials.json', 'r')
+    # try:
+    load = json.load(file)
+    user = load.get('user')
+    password = load.get('password')
+    file.close()
+    driver.find_elements(By.CLASS_NAME, "form-input")[0].send_keys(user)
+    driver.find_elements(By.CLASS_NAME, "form-input")[1].send_keys(password)
+    time.sleep(0.5)
+    driver.find_elements(By.CLASS_NAME, "btn")[1].click()
+except:
+    print(Fore.GREEN + "Log in to continue...")
 
 while(driver.current_url != 'https://app.usertesting.com/my_dashboard/available_tests_v3'):
     time.sleep(1)
@@ -35,7 +60,7 @@ last_title = "Available tests - UserTesting"
 
 while (True):
     time.sleep(20)
-    if ((last_title != driver.title) and (driver.title[0:1] == '(') and (driver.title[1:2] != '0')):
+    if ((last_title != driver.title) and (driver.title[0:1] == '(') and (driver.title[1:2] > last_title[1:2])):
         print(Fore.BLUE + datetime.now().strftime("%H:%M:%S") + ": NEW TEST AVAILABLE: " + Fore.RED + driver.title[1:2])
         notification.notify(
             title="UTnotifier",
