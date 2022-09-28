9bdb0f30831521f8f29968c461de90474845726d
pacjo <kamil.kroliczek2004@gmail.com>
2022-09-28 20:22:46 +0200

    changed notification handling, tried to fix login issues

--- notifier.py
+++ notifier.py
@@ -5,6 +5,7 @@ import argparse
 from gotify import Gotify
 import paho.mqtt.client as mqtt
 from colorama import init, Fore
+from plyer import notification
 
 # Script path
 filename = inspect.getframeinfo(inspect.currentframe()).filename
@@ -14,23 +15,22 @@ file_path = os.path.dirname(os.path.abspath(filename))
 parser = argparse.ArgumentParser(description='Very simple MQTT publisher for UTnotifier')
 parser.add_argument('payload', help='The payload to publish')
 parser.add_argument('filepath', nargs='?', default=file_path, help='Filepath of UTnotifier.py')
+parser.add_argument('--mqtt', action='store_true', help='Send MQTT notification')
+parser.add_argument('--gotify', action='store_true', help='Send Gotify notification')
+parser.add_argument('--local', action='store_true', help='Send local (PC) notification')
 args = parser.parse_args()
 
 # Initialise Colorama
 init(autoreset=True)
 
-def send_mqtt():
-    # JSON
-    file = open(f'{args.filepath}\credentials.json', 'r')
-    load = json.load(file)
-    json_mqtt = load.get('mqtt')
-    host = json_mqtt['host']
-    port = json_mqtt['port']
-    tls = json_mqtt['tls']               # currently not used
-    username = json_mqtt['username']
-    password = json_mqtt['password']
-    topic = json_mqtt['topic']
+def send_local(title, message, icon):
+    notification.notify(
+        title=title,
+        message=message,
+        app_icon=icon
+    )
 
+def send_mqtt(title, payload, host, port, tls, username, password, topic):
     # MQTT connection
     def on_connect(client, userdata, flags, rc):
         print("Connected with result code " + str(rc))
@@ -44,7 +44,7 @@ def send_mqtt():
         client.connect(host, port)
 
         # Publish message
-        client.publish(topic, payload=args.payload)
+        client.publish(topic, payload=payload)
         print(Fore.GREEN + "MQTT message published")
 
         client.disconnect()
@@ -66,11 +66,22 @@ def send_gotify(title, message, base_url, app_token, priority=0):
         priority=0,
     )
 
-# JSON
+# Get data from JSON file
 file = open(f'{args.filepath}\credentials.json', 'r')
 load = json.load(file)
-json_gotify = load.get('gotify')
-gotify_url = json_gotify['url']
-gotify_token = json_gotify['token']
 
-send_gotify("UTnotifier", args.payload, gotify_url, gotify_token)
+if(args.mqtt): 
+    json_mqtt = load.get('mqtt')
+    mqtt_host = json_mqtt['host']
+    mqtt_port = json_mqtt['port']
+    mqtt_tls = json_mqtt['tls']               # currently not used
+    mqtt_username = json_mqtt['username']
+    mqtt_password = json_mqtt['password']
+    mqtt_topic = json_mqtt['topic']
+    send_mqtt("UTnotifier", args.payload, mqtt_host, mqtt_port, mqtt_tls, mqtt_username, mqtt_password, mqtt_topic)
+if(args.gotify): 
+    json_gotify = load.get('gotify')
+    gotify_url = json_gotify['url']
+    gotify_token = json_gotify['token']
+    send_gotify("UTnotifier", args.payload, gotify_url, gotify_token)
+if(args.local): send_local("UTnotifier", args.payload, f'{file_path}\\assets\\ut_icon.ico')
