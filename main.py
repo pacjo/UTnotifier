f26beca0d1988f727026aadc748fca6aae330b42
pacjo <kamil.kroliczek2004@gmail.com>
2023-10-08 20:50:44 +0200

    Added back banner and colorama

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import os, os.path
 import inspect
 import json
+from colorama import init, Fore
 
 from login import login
 from monitor import monitor_title
 from notify import apprise_notify
 
 if __name__ == '__main__':
+
+    # Initialise Colorama
+    init(autoreset=True)
+
+    print(Fore.BLUE + "  _   _ _____            _   _  __ _            ")
+    print(Fore.BLUE + " | | | |_   _| __   ___ | |_(_)/ _(_) ___ _ __  ")
+    print(Fore.BLUE + " | | | | | || '_ \ / _ \| __| | |_| |/ _ \ '__| ")
+    print(Fore.BLUE + " | |_| | | || | | | (_) | |_| |  _| |  __/ |    ")
+    print(Fore.BLUE + "  \___/  |_||_| |_|\___/ \__|_|_| |_|\___|_|    ")
+    print(Fore.MAGENTA + "\n https://github.com/pacjo/UTnotifier \n")
+
     with open('credentials.json', 'r') as file:
         load = json.load(file)
         username = load.get('user')
